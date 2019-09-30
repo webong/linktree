5cdf079de4ca5eddc1aac44f3c6666f668f52334
wisdomanthoni <wizitendo10@gmail.com>
2019-09-30 01:30:24 +0100

    read image fills from page selections and decode the byte to image

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template>
   <div>
-    <img src="./logo.svg" alt="Logo">
-    <h2>Rectangle Creator</h2>
-    <p>
-      Count:
-      <input type="number" v-model.number="count" value="2" />
-    </p>
-    <button @click="create">Create</button>
+    <h2>Link Tree</h2>
+    <div>
+        <img :src="image" width="200" height="250" />
+    </div>
     <button @click="cancel">Cancel</button>
   </div>
 </template>
@@ -16,6 +13,7 @@ export default {
   name: "App",
   data() {
     return {
+      image: null,
       count: 2
     };
   },
@@ -30,9 +28,51 @@ export default {
     },
     cancel: function() {
       parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
+    },
+    // Decoding an image can be done by sticking it in an HTML
+    // canvas, as we can read individual pixels off the canvas.
+    decode: async function (canvas, ctx, bytes) {
+      const url = URL.createObjectURL(new Blob([bytes]))
+      const image = await new Promise((resolve, reject) => {
+        const img = new Image()
+        img.onload = () => resolve(img)
+        img.onerror = () => reject()
+        img.src = url
+      })
+      // canvas.width = image.width
+      // canvas.height = image.height
+      // ctx.drawImage(image, 0, 0)
+      // const imageData = ctx.getImageData(0, 0, image.width, image.height)
+      return image
     }
   },
-  created() {}
+  mounted() {
+    window.onmessage = async (event) => {
+      // Just get the bytes directly from the pluginMessage since
+      // that's the only type of message we'll receive in this
+      // plugin. In more complex plugins, you'll want to check the
+      // type of the message.
+      const bytes = event.data.pluginMessage
+
+      const canvas = document.createElement('canvas')
+      const ctx = canvas.getContext('2d')
+
+      const image = await this.decode(canvas, ctx, bytes)
+      // const pixels = imageData.data
+      console.log(image)
+      this.image = image.src
+      // Do the actual work of inverting the colors.
+      // for (let i = 0; i < pixels.length; i += 4) {
+      //   pixels[i + 0] = 255 - pixels[i + 0]
+      //   pixels[i + 1] = 255 - pixels[i + 1]
+      //   pixels[i + 2] = 255 - pixels[i + 2]
+      //   // Don't invert the alpha channel.
+      // }
+
+      // const newBytes = await encode(canvas, ctx, imageData)
+      // window.parent.postMessage({pluginMessage: newBytes}, '*')
+    }
+  }
 };
 </script>
 
@@ -44,5 +84,42 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  font: 12px sans-serif;
+  text-align: center;
+  margin: 20px;
+}
+button {
+  border-radius: 5px;
+  background: white;
+  color: black;
+  border: none;
+  padding: 8px 15px;
+  margin: 0 5px;
+  box-shadow: inset 0 0 0 1px black;
+  outline: none;
+}
+#create {
+  box-shadow: none;
+  background: #18a0fb;
+  color: white;
+}
+input {
+  border: none;
+  outline: none;
+  padding: 8px;
+}
+input:hover {
+  box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.1);
+}
+button:focus {
+  box-shadow: inset 0 0 0 2px #18a0fb;
+}
+#create:focus {
+  box-shadow: inset 0 0 0 2px rgba(0, 0, 0, 0.3);
+}
+input:focus {
+  box-shadow: inset 0 0 0 2px #18a0fb;
 }
 </style>
--- src/code.ts
+++ src/code.ts
@@ -6,28 +6,70 @@
 // full browser enviroment (see documentation).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__);
+// figma.showUI(__html__);
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
 // posted message.
+// figma.ui.onmessage = msg => {
+//   // One way of distinguishing between different types of messages sent from
+//   // your HTML page is to use an object with a "type" property like this.
+//   if (msg.type === 'create-rectangles') {
+//     const nodes: SceneNode[] = [];
+//     for (let i = 0; i < msg.count; i++) {
+//       const rect = figma.createRectangle();
+//       rect.x = i * 150;
+//       rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+//       figma.currentPage.appendChild(rect);
+//       nodes.push(rect);
+//     }
+//     figma.currentPage.selection = nodes;
+//     figma.viewport.scrollAndZoomIntoView(nodes);
+//   }
+
+//   // Make sure to close the plugin when you're done. Otherwise the plugin will
+//   // keep running, which shows the cancel button at the bottom of the screen.
+//   figma.closePlugin();
+// };
+
+async function invertImages(node) {
+  if (node.fills.length === 0) {
+    figma.closePlugin("Selection is not an image asset");
+  }
+  for (const paint of node.fills) {
+    if (paint.type !== 'IMAGE') {
+      figma.closePlugin("Selection is not an image asset");
+    } else {
+      // Get the (encoded) bytes for this image.
+      const image = figma.getImageByHash(paint.imageHash)
+      const bytes = await image.getBytesAsync()
+
+      // Create an invisible iframe to act as a "worker" which
+      // will do the task of decoding and send us a message
+      // when it's done. This shows the HTML page in "ui.html".
+      figma.showUI(__html__, {
+        width: 600,
+        height: 300
+      })
+
+      // Send the raw bytes of the file to the worker.
+      figma.ui.postMessage(bytes)
+    }
+  }
+}
+
+const selected = figma.currentPage.selection[0] as GeometryMixin
+console.log(selected)
+if (selected === undefined || selected.fills === undefined) {
+  figma.closePlugin("Selection is not an image asset");
+}
+invertImages(selected)
+
 figma.ui.onmessage = msg => {
   // One way of distinguishing between different types of messages sent from
   // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+  if (msg.type === 'cancel') {
+    // This closes the plugin when the cancel button is clicked.
+    figma.closePlugin();
   }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
 };
